2dc1b4db932c7568c75428ba396ba9fd8307190b
Deep13 <deep.deepak30@gmail.com>
2024-05-01 20:52:22 +0530

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,83 @@
+export const day_title = 'Happy Ram Navami';
+export const banner_key1 = 'ca-app-pub-4717579333914549/6573398002';
+export const banner_key2 = 'ca-app-pub-4717579333914549/2634152994';
+export const banner_key3 = 'ca-app-pub-4717579333914549/4976411156';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/3755662972';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/3564091281';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/3372519590';
+export const language = ['English', 'Hindi'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'Ram Navami wishes to you and your family. May the almighty bless you with good health and success. Happy Ram Navami.',
+      },
+      {
+        id: '002',
+        text:
+          'On this occasion, I pray that the blessings of Lord Ram be with you and your life be filled with happiness, peace and prosperity. Happy Ram Navami.',
+      },
+      {
+        id: '003',
+        text:
+          'May this auspicious day bring you success and happiness. Wish you a very happy Ram Navami',
+      },
+      {
+        id: '004',
+        text:
+          'May the almighty always be with you. Wish you a very happy and prosperous Ram Navami.',
+      },
+      {
+        id: '005',
+        text:
+          'May this holy occasion of Ram Navami bring lot of positivity, harmony and peace in your life. Happy Ram Navami!',
+      },
+      {
+        id: '006',
+        text:
+          'May the gleam of diyas and the echo of the chants fill your life with happiness and contentment.',
+      },
+      {
+        id: '007',
+        text: 'Wishing you a Happy Ram Navami.',
+      },
+      {
+        id: '008',
+        text:
+          'On this holy occasion of Rama Navami, I am wishing that blessings of Shri Ram be with you. And your heart and home be filled with happiness, peace and prosperity.',
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'श्री रामचंद्र कृपालु भज, मन हरण भवभय दारुणम्। नवकंज लोचन, कंज मुख, कर कंज, पद कंजारुणम्। -रामनवमी की शुभकामनाएं!',
+      },
+      {
+        id: '002',
+        text:
+          'राम जी की निकली सवारी, राम जी की लीला है न्यारी-न्यारी, एक तरफ लक्ष्मण एक तरफ सीता बीच में जगत के पालनहारी। -रामनवमी की शुभकामनाएं!',
+      },
+      {
+        id: '003',
+        text:
+          'गुणवान तुम, बलवान तुम, भक्‍तों को देते हो वरदान तुम, भगवान तुम, पालनहार तुम, मुश्किल को कर देते आसान तुम ! जय श्री राम ! -रामनवमी की शुभकामनाएं!',
+      },
+      {
+        id: '004',
+        text: 'रामनवमी की शुभकामनाएं!',
+      },
+      {
+        id: '005',
+        text:
+          'राम जिनका नाम है, अयोध्या जिनका धाम है, ऐसे रघुनंदन को, आपको और आपके परिवार को राम नवमी की हार्दिक शुभकामनाएं!',
+      },
+    ]
+  }
+
+};
